d472354ce8c335c344d40df9c0b96d89d8abfa4f
dam-brz <dam-brz@wp.pl>
2021-08-29 12:52:44 +0200

    Initial commit

---
+++ src/main/java/com/dambrz/ecommercbookstoreapi/EcommercBookStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.dambrz.ecommercbookstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercBookStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercBookStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dambrz/ecommercbookstoreapi/EcommercBookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dambrz.ecommercbookstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercBookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
